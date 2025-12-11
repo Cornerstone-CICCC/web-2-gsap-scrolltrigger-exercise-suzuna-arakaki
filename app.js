ba83056ad1a392977999f00bce5f8402c72e5bc5
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-12-10 23:24:40 -0800

    exercise is edite

--- app.js
+++ app.js
@@ -1,3 +1,62 @@
 gsap.registerPlugin(ScrollTrigger)
 
 // YOUR CODE HERE
+
+const lineOneTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".line-one",
+    start: "top top",
+    end: "bottom top",
+    pin: true,
+    scrub: true,
+  }
+})
+
+lineOneTimeline.from(".line-one span:first-child", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-one span:nth-child(2)", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-one span:nth-child(3)", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-one span:nth-child(4)", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-one span:nth-child(5)", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-one span:nth-child(6)", { y: "-100vh", opacity: 0, duration: 1 })
+  .to(".line-one", { backgroundColor: "yellow", duration: 1 })
+  .from(".sun", { x: "100vw", duration: 1 })
+  .to(".line-one", { backgroundColor: "white", duration: 1 })
+  .to(".line-one span", { x: "100vw", duration: 1 }, "<")
+  .to(".sun", { x: "-100vw", duration: 1 }, "<")
+
+
+
+const lineTwoTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".line-two",
+    start: "top top",
+    end: "bottom center",
+    pin: true,
+    scrub: true,
+  }
+})
+
+lineTwoTimeline.from(".line-two span", {
+  scale: 50,
+  opacity: 0,
+  duration: 1,
+})
+
+
+
+const lineThreeTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".line-three",
+    start: "top top",
+    end: "bottom top",
+    pin: true,
+    scrub: true,
+  }
+})
+
+lineThreeTimeline.from(".line-three span:first-child", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-three span:nth-child(2)", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-three span:nth-child(3)", { y: "-100vh", opacity: 0, duration: 1 })
+  .from(".line-three span:nth-child(4)", { y: "-100vh", opacity: 0, duration: 1 })
+  .to(".line-three", { backgroundPositionX: "0%", color: "white", duration: 1 })
+  .from(".moon", { x: "100vw", duration: 1 })
